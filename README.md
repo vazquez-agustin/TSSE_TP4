f6efbb6bf4eb8d8390a08c2f47055626914d5ec4
Agustin Vazquez <vazquezagostino@gmail.com>
2025-04-24 22:52:44 +0100

    Integration proof test (#1)
    * validations file created
    
    * README update
    
    * Add file to support folder
    
    * Fromat verification
    
    * mail update on license
    
    * clang format

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Testing de Software de Sistemas Embebidos
 
-## Trabajo Práctico N°2 - Ceedling
+## Trabajo Práctico N°4 - Integración continua
 
 ### Agustin Jesus Vazquez
--- src/leds.c
+++ src/leds.c
@@ -1,5 +1,5 @@
 /************************************************************************************************
-Copyright (c) 2025, Agustin Jesus Vazquez <vazqueza193@gmail.com>
+Copyright (c) 2025, Agustin Jesus Vazquez <vazquezagostino@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
